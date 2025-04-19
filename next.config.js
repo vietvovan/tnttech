1166d9abc1bda2cb9ad98132abb3493f5202e151
vietvovan <anhvietkienxuong@gmail.com>
2025-04-20 00:35:53 +0700

    test server

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compress: true,
+  productionBrowserSourceMaps: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  }
 }
 
 module.exports = nextConfig 
